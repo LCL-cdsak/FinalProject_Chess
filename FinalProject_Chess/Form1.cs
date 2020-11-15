419dc2e96671c6e3c29f2ac8c8c030b296803235
LCL-cdsak <patrick881212@gmail.com>
2020-11-16 01:03:27 +0800

    commit

--- FinalProject_Chess/Form1.cs
+++ FinalProject_Chess/Form1.cs
@@ -45,6 +45,7 @@ namespace FinalProject_Chess
         public PictureBox bkishop = new PictureBox();
         public PictureBox bkook = new PictureBox();
         public PictureBox bpawn = new PictureBox();
+        public PictureBox piece;
         public Point[,] point = new Point[8, 8];
         public Panel table = new Panel();
         public bool _MouseDown = false;
@@ -68,7 +69,6 @@ namespace FinalProject_Chess
 
         private void Table_MouseDown(object sender, MouseEventArgs e)
         {
-            PictureBox piece = (PictureBox)sender;
             if (_MouseDown)
             {
                 Point p = table.PointToClient(Cursor.Position);
@@ -95,10 +95,10 @@ namespace FinalProject_Chess
             bknight.BringToFront();
             bknight.BackColor = Color.Transparent;
             bknight.MouseClick += new MouseEventHandler(knightclicked);
-  
-        }
+          }
         private void knightclicked(object sender, MouseEventArgs e)
         {
+            piece = (PictureBox)sender;
             if (_MouseDown)
             {
                 _MouseDown = false;
