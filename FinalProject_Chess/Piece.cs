fe51e9e728043e123206ae31ab986359a27c8611
nckuwater <f74081195@gs.ncku.edu.tw>
2020-11-16 00:17:33 +0800

    Add Piece.cs

---
+++ FinalProject_Chess/Piece.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject_Chess
+{
+    class Piece
+    {
+        /*
+         * 白方:
+         * p:士兵
+         * r:城堡
+         * h:騎士
+         * b:主教
+         * q:皇后
+         * k:國王
+         * 黑方:
+         * P:士兵
+         * R:城堡
+         * H:騎士
+         * B:主教
+         * Q:皇后
+         * K:國王 
+         * 
+         * N:空格
+         */
+    }
+    enum PieceType
+    {
+        wPawn,
+        wRook,
+        wKnight,
+        wBishop,
+        wQueen,
+        wKing,
+        bPawn,
+        bRook,
+        bKnight,
+        bBishop,
+        bQueen,
+        bKing
+    }
+}
